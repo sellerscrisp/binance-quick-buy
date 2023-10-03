1cfd0108f73b3373e39dd73f98862176f23c5633
shcrisp <sellershcrisp@gmail.com>
2023-10-02 21:06:58 -0400

    refactored

--- main.py
+++ main.py
@@ -1,99 +1,81 @@
-# built-in
+import logging
 from decimal import Decimal, ROUND_DOWN
+from typing import Dict, Union
 
-# third-party
 from binance.client import Client
-from binance.exceptions import BinanceOrderException
 from binance.enums import SIDE_BUY, SIDE_SELL, ORDER_TYPE_MARKET, ORDER_TYPE_LIMIT, TIME_IN_FORCE_GTC
+from binance.exceptions import BinanceOrderException
 
-# local
 from settings import API_KEY, API_SECRET
 
-client = Client(API_KEY, API_SECRET)
-
-class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def color_print(color, text):
-    print(color + text + Colors.ENDC)
+class BinanceTrader:
+    
+    def __init__(self):
+        self.client = Client(API_KEY, API_SECRET)
+        self.btc_balance = self.get_btc_balance()
+        self.currency = ""
+        self.symbol = ""
+        self.minimum = 0.0
+        self.price_jump = 0.0
+        self.buy_price = 0.0
+        self.quantity = 0.0
 
+    def get_btc_balance(self) -> float:
+        return float(self.client.get_account()['balances'][0]['free'])
 
-def get_btc_balance():
-    return float(client.get_account()['balances'][0]['free'])
+    def get_symbol_info(self) -> Dict[str, Union[str, float]]:
+        return self.client.get_symbol_info(self.symbol)
 
+    def calculate_quantity(self) -> Decimal:
+        if self.minimum < 1:
+            return Decimal(self.btc_balance / self.buy_price).quantize(Decimal(str(self.minimum)), rounding=ROUND_DOWN)
+        return Decimal(self.btc_balance / self.buy_price).quantize(Decimal('1.'), rounding=ROUND_DOWN)
 
-def get_symbol_info(symbol):
-    return client.get_symbol_info(symbol)
+    def create_order(self, side: str, order_type: str, price: float = None):
+        order = {
+            "symbol": self.symbol,
+            "side": side,
+            "type": order_type,
+            "quantity": self.quantity
+        }
+        if price:
+            order["price"] = price
+            order["timeInForce"] = TIME_IN_FORCE_GTC
+        self.client.create_test_order(**order)
 
+    def initialize_trade(self):
+        self.currency = input("Enter a coin abbreviation: ").upper()
+        logger.info(f"Using your full BTC balance: {self.btc_balance}")
+        self.symbol = f"{self.currency}BTC"
+        info = self.get_symbol_info()
+        self.minimum = float(info['filters'][2]['minQty'])
+        self.price_jump = float(info['filters'][0]['tickSize'])
+        self.buy_price = float(self.client.get_orderbook_ticker(symbol=self.symbol)['askPrice'])
+        self.quantity = self.calculate_quantity()
 
-def calculate_quantity(btc_amount, buy_price, minimum):
-    if minimum < 1:
-        return Decimal(btc_amount / buy_price).quantize(Decimal(str(minimum)), rounding=ROUND_DOWN)
-    else:
-        return Decimal(btc_amount / buy_price).quantize(Decimal('1.'), rounding=ROUND_DOWN)
+    def execute_trade(self):
+        try:
+            self.create_order(SIDE_BUY, ORDER_TYPE_MARKET)
+            logger.info(f'BUY order of {self.quantity} {self.symbol} set')
+        except BinanceOrderException as e:
+            logger.error(f"{e.status_code} - {e.message}")
 
-
-def create_order(symbol, side, order_type, quantity, price=None):
-    order = {
-        "symbol": symbol,
-        "side": side,
-        "type": order_type,
-        "quantity": quantity
-    }
-    if price:
-        order["price"] = price
-        order["timeInForce"] = TIME_IN_FORCE_GTC
-
-    client.create_test_order(**order)
-
-
-def create_buy_order(symbol, quantity):
-    try:
-        create_order(symbol, SIDE_BUY, ORDER_TYPE_MARKET, quantity)
-        color_print(Colors.HEADER, '# ------------------------------------------------------------------------- #')
-        color_print(Colors.OKGREEN, f'BUY order of {quantity} {symbol} set @ {quantity} BTC totalling {quantity} BTC')
-    except BinanceOrderException as e:
-        color_print(Colors.FAIL, str(e.status_code))
-        color_print(Colors.FAIL, e.message)
-
-
-def create_sell_order(symbol, quantity, sell_price):
-    try:
-        create_order(symbol, SIDE_SELL, ORDER_TYPE_LIMIT, quantity, sell_price)
-        color_print(Colors.HEADER, '# ------------------------------------------------------------------------- #')
-        color_print(Colors.OKCYAN, f'SELL order of {quantity} {symbol} set @ {sell_price} BTC totalling {sell_price*quantity} BTC')
-    except BinanceOrderException as e:
-        color_print(Colors.FAIL, str(e.status_code))
-        color_print(Colors.FAIL, e.message)
+        sell_price = Decimal(1.5 * self.buy_price).quantize(Decimal(str(self.price_jump)), rounding=ROUND_DOWN)
+        try:
+            self.create_order(SIDE_SELL, ORDER_TYPE_LIMIT, sell_price)
+            logger.info(f'SELL order of {self.quantity} {self.symbol} set')
+        except BinanceOrderException as e:
+            logger.error(f"{e.status_code} - {e.message}")
 
 
 def main():
-    color_print(Colors.BOLD + Colors.OKBLUE, 'Enter a coin abbreviation: ')
-    currency = input().upper()
-    btc_balance = get_btc_balance()
-    color_print(Colors.FAIL, f'Using your full BTC balance: {btc_balance}')
-
-    symbol = currency + 'BTC'
-    info = get_symbol_info(symbol)
-
-    minimum = float(info['filters'][2]['minQty'])
-    price_jump = float(info['filters'][0]['tickSize'])
-    buy_price = float(client.get_orderbook_ticker(symbol=symbol)['askPrice'])
-    quantity = calculate_quantity(btc_balance, buy_price, minimum)
-
-    create_buy_order(symbol, quantity)
-
-    sell_price = Decimal(1.5 * buy_price).quantize(Decimal(str(price_jump)), rounding=ROUND_DOWN)
-    create_sell_order(symbol, quantity, sell_price)
+    trader = BinanceTrader()
+    trader.initialize_trade()
+    trader.execute_trade()
 
 
 if __name__ == "__main__":
